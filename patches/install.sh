f5ea433a11876140d46e800764820fe16542082b
Abhishek AN <basevd94@gmail.com>
2017-09-03 00:27:40 +0530

    1 Patch Directory

--- patches/install.sh
+++ patches/install.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic frameworks/av packages/apps/Settings system/core "
+dirs="bionic frameworks/av frameworks/base system/core system/netd "
 
 for dir in $dirs ; do
 	cd $rootdirectory
